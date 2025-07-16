2cb79a7c4c2cf7aec4b00aa2781a853da96abacc
JuliannaAreias <julianna_areias@id.uff.br>
2025-07-16 12:40:26 -0300

    Corrige porta e host para deploy no Render

--- app.py
+++ app.py
@@ -147,4 +147,8 @@ def profissional():
 
 # Executa o app Flask
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
